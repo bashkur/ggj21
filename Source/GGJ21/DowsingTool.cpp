9de78368bb10dc795c6bc77bf60e2b6303b3defb
Zachary Starkey <zstarkey@icloud.com>
2021-01-31 01:40:37 -0500

    Auto stash before merge of "main" and "origin/main"
    Added functionality to Dowsing Stick

--- Source/GGJ21/DowsingTool.cpp
+++ Source/GGJ21/DowsingTool.cpp
@@ -2,6 +2,7 @@
 
 
 #include "DowsingTool.h"
+#include "DrawDebugHelpers.h"
 
 void UDowsingTool::BeginPlay()
 {
@@ -16,4 +17,37 @@ void UDowsingTool::TickComponent(float DeltaTime, ELevelTick TickType, FActorCom
 void UDowsingTool::Use()
 {
 	V_LOG("Using Dowsing Tool!");
+	FVector Loc;
+	FRotator Rot;
+	FHitResult Hit;
+
+	APlayerController* player = UGameplayStatics::GetPlayerController(GetWorld(),0);
+	player->GetPlayerViewPoint(Loc,Rot);
+	TSet<UActorComponent*> components = player->GetOwner()->GetComponents();
+	UAudioComponent* sound = nullptr;
+	for (UActorComponent* c : components)
+	{
+		if (c->ComponentHasTag("Stick_Audio"))
+		{
+			sound = Cast<UAudioComponent>(c);
+		}
+	}
+	FVector Start = Loc;
+	FVector End = Start + (Rot.Vector()*2000);
+	FCollisionQueryParams TraceParams;
+	GetWorld()->LineTraceSingleByChannel(Hit,Start,End,ECC_Visibility,TraceParams);
+
+	DrawDebugLine(GetWorld(),Start,End,FColor::Blue,false,5.0f);
+	if (Hit.bBlockingHit)
+	{
+		V_LOG("Hit Something");
+		if (Hit.GetComponent()->ComponentHasTag("Treasure_Ring"))
+		{
+			V_LOG("Found Treasure Ring!");
+			if (sound)
+			{
+				sound->Play();
+			}
+		}
+	}
 }
